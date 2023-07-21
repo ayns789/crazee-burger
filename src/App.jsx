7cf0de28cd9ec8f567184a570ffd870f2872ada6
github_ayns789 <joly.a.idf@gmail.com>
2023-07-21 13:51:53 +0200

    > App.js > Add my firstname

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>Salut Ayns !</p>
+        <p>Salut Ayns</p>
       </div>
       <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </>
