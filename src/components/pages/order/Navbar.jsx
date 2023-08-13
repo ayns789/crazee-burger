e20922a3bb1cdad784dbfb0d12fa16dd0f549996
github_ayns789 <joly.a.idf@gmail.com>
2023-08-13 13:07:09 +0200

    🚧  F04: ORDER > layout > navbar > add left-side and right-side

--- src/components/pages/order/Navbar.jsx
+++ src/components/pages/order/Navbar.jsx
@@ -6,12 +6,15 @@ export default function Navbar() {
   return (
     <>
       <NavbarStyled>
-        <h1>
-          Bonjour <UsernameStyled>{username}</UsernameStyled>
-        </h1>
-        <Link to='/'>
-          <button>Déconnexion</button>
-        </Link>
+        <div className='left-side'>Left</div>
+        <div className='right-side'>
+          <h1>
+            Bonjour <UsernameStyled>{username}</UsernameStyled>
+          </h1>
+          <Link to='/'>
+            <button>Déconnexion</button>
+          </Link>
+        </div>
       </NavbarStyled>
     </>
   );
@@ -23,4 +26,16 @@ const UsernameStyled = styled.span`
 
 const NavbarStyled = styled.div`
   background: blue;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 10vh;
+  .left-side {
+    background-color: antiquewhite;
+  }
+  .right-side {
+    background-color: aliceblue;
+    h1 {
+    }
+  }
 `;
