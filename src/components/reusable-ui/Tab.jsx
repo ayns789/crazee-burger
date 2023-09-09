987ad4c55b49fd568472c6a49385d2421791e292
github_ayns789 <joly.a.idf@gmail.com>
2023-09-09 13:13:22 +0200

    💄 F07: ADMIN > add Tab

---
+++ src/components/reusable-ui/Tab.jsx
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+import { theme } from '../../theme';
+import PropTypes from 'prop-types';
+
+export default function Tab({ Icon }) {
+  return (
+    <TabStyled>
+      <div className='icon'>{Icon}</div>
+    </TabStyled>
+  );
+}
+
+Tab.propTypes = {
+  Icon: PropTypes.object,
+};
+
+const TabStyled = styled.button`
+  height: 43px;
+  padding: 0 22px;
+  cursor: pointer;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  position: relative;
+  left: 5%;
+  top: 1px;
+
+  font-size: ${theme.fonts.size.P0};
+  color: ${theme.colors.greySemiDark};
+
+  background: ${theme.colors.white};
+  box-shadow: ${theme.shadows.subtle};
+
+  border-width: 1px 1px 2px 1px;
+  border-style: solid;
+  border-color: ${theme.colors.greyLight};
+  border-radius: 5px 5px 0px 0px;
+
+  &:hover {
+    border-bottom: 2px solid ${theme.colors.white};
+  }
+
+  .icon {
+    display: flex;
+  }
+`;
