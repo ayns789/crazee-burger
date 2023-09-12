32c879b3701475a8d7c4be063109594ced30c88e
github_ayns789 <joly.a.idf@gmail.com>
2023-09-13 00:24:23 +0200

    💄 F07: Theme > add shadows

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -25,7 +25,7 @@ const MainStyled = styled.div`
   height: calc(95vh - 10vh);
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
-  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  box-shadow: ${theme.shadows.strong};
   display: grid;
   grid-template-columns: 1fr;
   // this code will be use for adding basket
