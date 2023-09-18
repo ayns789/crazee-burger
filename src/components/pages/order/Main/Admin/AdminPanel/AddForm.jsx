0f7bad483d9f7d03eed4d36ccc3a5d058af6f8cd
github_ayns789 <joly.a.idf@gmail.com>
2023-09-18 11:57:42 +0200

    ♻️ F08: ADMIN > AddForm > use crypto.randomUUID() and destructure function handleChange

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -22,20 +22,28 @@ export default function AddForm() {
 
     const newProductToAdd = {
       ...newProduct,
-      id: new Date().getTime(),
+      // id: new Date.getTime(),
+      id: crypto.randomUUID(),
     };
-
+    console.log('newProduct is : ', newProductToAdd);
     handleAdd(newProductToAdd);
   };
 
   const handleChange = (event) => {
     event.preventDefault();
-    // on récupère la valeur de tous les champs
-    const newValue = event.target.value;
-    // on récupère le nom de tous les champs
-    const name = event.target.name;
-    // on met à jour newProduct avec nom et valeur des champs
-    setNewProduct({ ...newProduct, [name]: newValue });
+    // // on récupère la valeur de tous les champs
+    // const newValue = event.target.value;
+    // // on récupère le nom de tous les champs
+    // const name = event.target.name;
+    // // on met à jour newProduct avec nom et valeur des champs
+    // setNewProduct({ ...newProduct, [name]: newValue });
+
+    // // en destructuring :
+    // const { name, value } = event.target;
+    // setNewProduct({ ...newProduct, [name]: value });
+
+    // autre façon de procéder :
+    setNewProduct({ ...newProduct, [event.target.name]: event.target.value });
   };
 
   return (
