8aa9e4433ae93923fabc8a1ef5e21a31c627be1d
github_ayns789 <joly.a.idf@gmail.com>
2023-09-18 11:33:24 +0200

    ♻️ F08: ADMIN > AddForm > Use of dynamic property name

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -1,30 +1,71 @@
 import styled from 'styled-components';
 import OrderContext from '../../../../../../context/OrderContext';
 import { useContext } from 'react';
+import { useState } from 'react';
+
+const EMPTY_PRODUCT = {
+  id: '',
+  title: '',
+  imageSource: '',
+  price: '',
+};
+
+// https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs0QTOAdZu3bcdNH0QqW8U2NpKmgkgqcY7-wRo3H4OKvki1XhcpFUDHoAHr-zgEr16zlE&usqp=CAU
 
 export default function AddForm() {
   const { handleAdd } = useContext(OrderContext);
 
-  const newProduct = {
-    id: new Date().getTime(),
-    title: 'Nouveau produit',
-    imageSource:
-      'https://img2.freepng.fr/20190625/skz/kisspng-cheeseburger-hamburger-patty-food-stock-photograph-5d12d8eec90486.6268925215615162708234.jpg',
-    price: 2.5,
+  const [newProduct, setNewProduct] = useState(EMPTY_PRODUCT);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const newProductToAdd = {
+      ...newProduct,
+      id: new Date().getTime(),
+    };
+
+    handleAdd(newProductToAdd);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    handleAdd(newProduct);
+  const handleChange = (event) => {
+    event.preventDefault();
+    // on récupère la valeur de tous les champs
+    const newValue = event.target.value;
+    // on récupère le nom de tous les champs
+    const name = event.target.name;
+    // on met à jour newProduct avec nom et valeur des champs
+    setNewProduct({ ...newProduct, [name]: newValue });
   };
 
   return (
     <AddFormStyled onSubmit={handleSubmit}>
       <div className='image-preview'>Image Preview</div>
       <div className='input-fields'>
-        <input type='text' placeholder='Nom du produit' />
-        <input type='text' placeholder='image url' />
-        <input type='text' placeholder='price' />
+        <input
+          key='title'
+          name='title'
+          type='text'
+          placeholder='Nom du produit'
+          value={newProduct.title}
+          onChange={handleChange}
+        />
+        <input
+          key='imageSource'
+          name='imageSource'
+          type='text'
+          placeholder='image url'
+          value={newProduct.imageSource}
+          onChange={handleChange}
+        />
+        <input
+          key='price'
+          name='price'
+          type='text'
+          placeholder='prix'
+          value={newProduct.price}
+          onChange={handleChange}
+        />
       </div>
       <div className='submit-button'>
         <button>Submit button</button>
