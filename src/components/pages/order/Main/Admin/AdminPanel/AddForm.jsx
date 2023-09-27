8f178ea20241544f03ea4756d4624a21d8200b4b
github_ayns789 <joly.a.idf@gmail.com>
2023-09-27 22:05:15 +0200

    💄 F08: AddForm > add submit message style

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -109,13 +109,13 @@ export default function AddForm() {
       <div className='submit'>
         <Button
           className='submit-button'
-          label={'Ajouter un nouveau produit'}
+          label={'Ajouter un nouveau produit au menu'}
           version={'minimalist'}
         />
         {isSubmitted && (
           <div className='submit-message'>
-            <FiCheck />
-            <span>Ajouté avec succès !</span>
+            <FiCheck className='icon' />
+            <span className='message'>Ajouté avec succès !</span>
           </div>
         )}
       </div>
@@ -167,7 +167,10 @@ const AddFormStyled = styled.form`
     grid-area: 4/2 / 5/3;
     display: flex;
     align-items: center;
-    height: 2.5em;
+    /* height: 2.5em; */
+    position: relative;
+    top: 3px;
+
     .submit-button {
       width: 50%;
       height: 100%;
@@ -179,6 +182,22 @@ const AddFormStyled = styled.form`
       justify-content: center;
       align-items: center;
       height: 100%;
+
+      .icon {
+        color: ${theme.colors.success};
+        margin-left: 10px;
+        width: 1em;
+        height: 1em;
+        border: 1px solid ${theme.colors.success};
+        border-radius: 50%;
+        vertical-align: middle;
+      }
+
+      .message {
+        margin-left: 5px;
+        font-size: ${theme.fonts.size.XS};
+        color: ${theme.colors.success};
+      }
     }
   }
 `;
